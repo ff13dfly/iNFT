98f0123f76c8f2c7592cd8a4d0cf4f02826a2268
fuu <fu@yihaofang.com>
2024-05-02 11:32:53 +0800

    1.progress panel UI done;

--- mint/src/component/progress.js
+++ mint/src/component/progress.js
@@ -1,11 +1,13 @@
 import { Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
+import { FaIdBadge } from "react-icons/fa";
+
 function Progress(props) {
     const size = {
         row: [12],
-        offset:[1,9,2],
-        multi:[4,4,4]
+        bar:[2,8,2],
+        step:[1,2,2,1,2],
     };
 
     let [list,setList]=useState([]);
@@ -19,10 +21,111 @@ function Progress(props) {
     }, [props.update]);
 
     return (
-        <Row className="pt-2">
-            <Col sm={size.row[0]} xs={size.row[0]}>
-                On progress list:
+        <Row>
+            <Col sm={size.row[0]} xs={size.row[0]}>10 mint, 4 done.</Col>
+            <Col className="pt-2" sm={size.row[0]} xs={size.row[0]}>
+                <Row>
+                    <Col className="pt-1" sm={size.bar[0]} xs={size.bar[0]}>
+                        <h5>#1</h5>
+                    </Col>
+                    <Col className="pt-1" sm={size.bar[1]} xs={size.bar[1]}>
+                        <Row className="pt-2">
+                            <Col className="pt-3 done" sm={size.step[0]} xs={size.step[0]}></Col>
+                            <Col className="pt-3 done" sm={size.step[1]} xs={size.step[1]}></Col>
+                            <Col className="pt-3 done" sm={size.step[2]} xs={size.step[2]}></Col>
+                            <Col className="pt-3 done" sm={size.step[3]} xs={size.step[3]}></Col>
+                            <Col className="pt-3 done" sm={size.step[4]} xs={size.step[4]}></Col>
+                            <Col className="pt-3 done"></Col>
+                        </Row>
+                    </Col>
+                    <Col className="pt-1 text-end" sm={size.bar[2]} xs={size.bar[2]}>
+                        <button className="btn btn-sm btn-secondary"><FaIdBadge /></button>
+                    </Col>
+                </Row>
             </Col>
+
+            <Col className="pt-2" sm={size.row[0]} xs={size.row[0]}>
+                <Row>
+                    <Col className="pt-1" sm={size.bar[0]} xs={size.bar[0]}>
+                        <h5>#2</h5>
+                    </Col>
+                    <Col className="pt-1" sm={size.bar[1]} xs={size.bar[1]}>
+                        <Row className="pt-2">
+                            <Col className="pt-3 done" sm={size.step[0]} xs={size.step[0]}></Col>
+                            <Col className="pt-3 done" sm={size.step[1]} xs={size.step[1]}></Col>
+                            <Col className="pt-3 going" sm={size.step[2]} xs={size.step[2]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[3]} xs={size.step[3]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[4]} xs={size.step[4]}></Col>
+                            <Col className="pt-3 waiting"></Col>
+                        </Row>
+                    </Col>
+                    <Col className="pt-1 text-end" sm={size.bar[2]} xs={size.bar[2]}>
+                        <button className="btn btn-sm btn-secondary"><FaIdBadge /></button>
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col className="pt-2" sm={size.row[0]} xs={size.row[0]}>
+                <Row>
+                    <Col className="pt-1" sm={size.bar[0]} xs={size.bar[0]}>
+                        <h5>#3</h5>
+                    </Col>
+                    <Col className="pt-1" sm={size.bar[1]} xs={size.bar[1]}>
+                        <Row className="pt-2">
+                            <Col className="pt-3 waiting" sm={size.step[0]} xs={size.step[0]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[1]} xs={size.step[1]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[2]} xs={size.step[2]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[3]} xs={size.step[3]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[4]} xs={size.step[4]}></Col>
+                            <Col className="pt-3 waiting"></Col>
+                        </Row>
+                    </Col>
+                    <Col className="pt-1 text-end" sm={size.bar[2]} xs={size.bar[2]}>
+                        <button className="btn btn-sm btn-secondary"><FaIdBadge /></button>
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col className="pt-2" sm={size.row[0]} xs={size.row[0]}>
+                <Row>
+                    <Col className="pt-1" sm={size.bar[0]} xs={size.bar[0]}>
+                        <h5>#4</h5>
+                    </Col>
+                    <Col className="pt-1" sm={size.bar[1]} xs={size.bar[1]}>
+                        <Row className="pt-2">
+                            <Col className="pt-3 waiting" sm={size.step[0]} xs={size.step[0]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[1]} xs={size.step[1]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[2]} xs={size.step[2]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[3]} xs={size.step[3]}></Col>
+                            <Col className="pt-3 waiting" sm={size.step[4]} xs={size.step[4]}></Col>
+                            <Col className="pt-3 waiting"></Col>
+                        </Row>
+                    </Col>
+                    <Col className="pt-1 text-end" sm={size.bar[2]} xs={size.bar[2]}>
+                        <button className="btn btn-sm btn-secondary"><FaIdBadge /></button>
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col className="pt-2" sm={size.row[0]} xs={size.row[0]}>
+                Info: every mint takes about 36 seconds to finalize.
+            </Col>
+            {/* <Col className="pt-2" sm={size.row[0]} xs={size.row[0]}>
+                <Row>
+                    <Col className="pt-1" sm={size.bar[0]} xs={size.bar[0]}></Col>
+                    <Col className="pt-1" sm={size.bar[1]} xs={size.bar[1]}>
+                        <Row className="pt-2">
+                            <Col className="text-center" sm={size.step[0]} xs={size.step[0]}>1</Col>
+                            <Col className="text-center" sm={size.step[1]} xs={size.step[1]}>2</Col>
+                            <Col className="text-center" sm={size.step[2]} xs={size.step[2]}>3</Col>
+                            <Col className="text-center" sm={size.step[3]} xs={size.step[3]}>4</Col>
+                            <Col className="text-center" sm={size.step[4]} xs={size.step[4]}>5</Col>
+                            <Col className="text-center">6</Col>
+                        </Row>
+                    </Col>
+                    <Col className="pt-1 text-end" sm={size.bar[2]} xs={size.bar[2]}></Col>
+                </Row>
+            </Col> */}
         </Row>
     )
 }
