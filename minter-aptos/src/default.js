eea939cf43538141cce562afcc6ce9a38d1c678f
fuu <fu@yihaofang.com>
2024-05-26 08:18:27 +0800

    1.remove backup; 2.remove test from minter;

--- minter-aptos/src/default.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    node:[
-        "ws://127.0.0.1:9944",
-        "wss://dev2.metanchor.net",
-    ],
-    default:[
-        "0xa69ddda382a348869159f1ed42eb2fd978a5a9b5e741a5f144be4b2ff9ffd069",
-    ],
-}
