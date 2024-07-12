e165f82f36a869e10ed19ef84d4e186cb64f520b
fuu <fu@yihaofang.com>
2024-07-12 23:51:02 +0800

    1. bounty UI improvement;

--- market/src/component/bounty_submit.js
+++ market/src/component/bounty_submit.js
@@ -1,7 +1,8 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Tabs, Tab } from 'react-bootstrap';
 import { useEffect, useState } from "react";
 
 import BountyTarget from './bounty_target';
+import BountyTemplate from './bounty_template';
 
 import TPL from "../system/tpl";
 
@@ -22,10 +23,13 @@ function BountySubmit(props) {
 
   //step enable
   let [ready, setReady] = useState(false);
-  let [pay,setPay]=useState(false);
+  let [pay, setPay] = useState(false);
 
   //sub component params
-  let [series, setSeries]= useState([]);
+  let [series, setSeries] = useState([]);
+  let [data, setData] = useState({});
+
+  //let [active, setActive]=useState("step_1");
 
   const self = {
     changeTitle: (ev) => {
@@ -41,117 +45,135 @@ function BountySubmit(props) {
       setTemplate(ev.target.value);
     },
     clickLoad: (ev) => {
-      TPL.view(template, (res) => {
-        console.log(res);
+      TPL.view(template, (dt) => {
+        setData(dt);
         setReady(true);
+        setSeries(dt.series);
       });
     },
   }
 
   useEffect(() => {
+    
   }, []);
 
   return (
-    <Row>
 
-      <Col md={size.step[0]} lg={size.step[0]} xl={size.step[0]} xxl={size.step[0]}>
-        <h4>Step 1</h4>
-      </Col>
-      <Col md={size.step[1]} lg={size.step[1]} xl={size.step[1]} xxl={size.step[1]}>
-        Select a template for bounty which is storage on IPFS.
-      </Col>
-
-      <Col className='pt-2' md={size.normal[0]} lg={size.normal[0]} xl={size.normal[0]} xxl={size.normal[0]}>
-        <input type="text" className='form-control' placeholder='Input template CID'
-          value={template} onChange={(ev) => {
-            self.changeTemplate(ev);
-          }} />
-      </Col>
-      <Col className='pt-2 text-end' md={size.normal[1]} lg={size.normal[1]} xl={size.normal[1]} xxl={size.normal[1]}>
-        <button className='btn btn-md btn-primary' onClick={(ev) => {
-          self.clickLoad(ev);
-        }}>Load</button>
-      </Col>
-      <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-        <hr />
-      </Col>
-
-      <Col hidden={!ready} md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+    <Tabs
+      defaultActiveKey="step_1"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="step_1" title="Step 1">
         <Row>
           <Col md={size.step[0]} lg={size.step[0]} xl={size.step[0]} xxl={size.step[0]}>
-            <h4>Step 2</h4>
+            <h4>Step 1</h4>
           </Col>
           <Col md={size.step[1]} lg={size.step[1]} xl={size.step[1]} xxl={size.step[1]}>
-            Setup the details about the bounty and write on chain.
+            Select a template for bounty which is storage on IPFS.
           </Col>
 
-          <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-            <small>The title of bounty</small>
-            <input type="text" className='form-control' placeholder='Input the title of bounty'
-              value={title} onChange={(ev) => {
-                self.changeTitle(ev);
+          <Col className='pt-2' md={size.normal[0]} lg={size.normal[0]} xl={size.normal[0]} xxl={size.normal[0]}>
+            <input type="text" className='form-control' placeholder='Input template CID'
+              value={template} onChange={(ev) => {
+                self.changeTemplate(ev);
               }} />
           </Col>
-
-          <Col className='pt-2' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-            <small>The title of bounty</small>
-            <textarea className='form-control' cols={4}></textarea>
+          <Col className='pt-2 text-end' md={size.normal[1]} lg={size.normal[1]} xl={size.normal[1]} xxl={size.normal[1]}>
+            <button className='btn btn-md btn-primary' onClick={(ev) => {
+              self.clickLoad(ev);
+            }}>Load</button>
           </Col>
-
-          <Col md={size.half[0]} lg={size.half[0]} xl={size.half[0]} xxl={size.half[0]}>
-            <small>Bounty start at block</small>
-            <input type="number" className='form-control' placeholder='Start of bounty'
-              value={start} onChange={(ev) => {
-                self.changeStart(ev);
-              }} />
-          </Col>
-
-          <Col md={size.half[0]} lg={size.half[0]} xl={size.half[0]} xxl={size.half[0]}>
-            <small>Bounty end at block</small>
-            <input type="number" className='form-control' placeholder='End of bounty'
-              value={end} onChange={(ev) => {
-                self.changeEnd(ev);
-              }} />
-          </Col>
-
-          <Col className='' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-            Current block number: {0}, {6}s per block.
-          </Col>
-
-          <Col className='' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-            <BountyTarget series={series} callback={() => { }} />
-          </Col>
-
-
-          <Col className='' md={size.normal[0]} lg={size.normal[0]} xl={size.normal[0]} xxl={size.normal[0]}>
-          </Col>
-          <Col className='text-end' md={size.normal[1]} lg={size.normal[1]} xl={size.normal[1]} xxl={size.normal[1]}>
-            <button className='btn btn-md btn-primary'>Submit</button>
-          </Col>
-
-          <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-            <hr />
+          <Col hidden={!ready} md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+            <BountyTemplate data={data} />
           </Col>
         </Row>
-      </Col>
-
-      <Col hidden={pay} md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+      </Tab>
+      <Tab eventKey="step_2" title="Step 2">
         <Row>
-          <Col md={size.step[0]} lg={size.step[0]} xl={size.step[0]} xxl={size.step[0]}>
-            <h4>Step 3</h4>
-          </Col>
-          <Col md={size.step[1]} lg={size.step[1]} xl={size.step[1]} xxl={size.step[1]}>
-            Pay the coins to target account.
-          </Col>
+          <Col hidden={!ready} md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+            <Row>
+              <Col md={size.step[0]} lg={size.step[0]} xl={size.step[0]} xxl={size.step[0]}>
+                <h4>Step 2</h4>
+              </Col>
+              <Col md={size.step[1]} lg={size.step[1]} xl={size.step[1]} xxl={size.step[1]}>
+                Setup the details about the bounty and write on chain.
+              </Col>
 
-          <Col className='' md={size.normal[0]} lg={size.normal[0]} xl={size.normal[0]} xxl={size.normal[0]}>
-          </Col>
-          <Col className='text-end' md={size.normal[1]} lg={size.normal[1]} xl={size.normal[1]} xxl={size.normal[1]}>
-            <button className='btn btn-md btn-primary'>Pay</button>
+              <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+                <small>The title of bounty</small>
+                <input type="text" className='form-control' placeholder='Input the title of bounty'
+                  value={title} onChange={(ev) => {
+                    self.changeTitle(ev);
+                  }} />
+              </Col>
+
+              <Col className='pt-2' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+                <small>The title of bounty</small>
+                <textarea className='form-control' cols={4}></textarea>
+              </Col>
+
+              <Col md={size.half[0]} lg={size.half[0]} xl={size.half[0]} xxl={size.half[0]}>
+                <small>Bounty start at block</small>
+                <input type="number" className='form-control' placeholder='Start of bounty'
+                  value={start} onChange={(ev) => {
+                    self.changeStart(ev);
+                  }} />
+              </Col>
+
+              <Col md={size.half[0]} lg={size.half[0]} xl={size.half[0]} xxl={size.half[0]}>
+                <small>Bounty end at block</small>
+                <input type="number" className='form-control' placeholder='End of bounty'
+                  value={end} onChange={(ev) => {
+                    self.changeEnd(ev);
+                  }} />
+              </Col>
+
+              <Col className='' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+                Current block number: {0}, {6}s per block.
+              </Col>
+
+              <Col className='' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+                <BountyTarget series={series} callback={() => { }} />
+              </Col>
+
+
+              <Col className='' md={size.normal[0]} lg={size.normal[0]} xl={size.normal[0]} xxl={size.normal[0]}>
+
+              </Col>
+              <Col className='text-end' md={size.normal[1]} lg={size.normal[1]} xl={size.normal[1]} xxl={size.normal[1]}>
+                <button className='btn btn-md btn-primary'>Submit</button>
+              </Col>
+
+              <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+                <hr />
+              </Col>
+            </Row>
           </Col>
         </Row>
-      </Col>
-    </Row>
+      </Tab>
+      <Tab eventKey="step_3" title="Step 3">
+        <Row>
+          <Col hidden={pay} md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+            <Row>
+              <Col md={size.step[0]} lg={size.step[0]} xl={size.step[0]} xxl={size.step[0]}>
+                <h4>Step 3</h4>
+              </Col>
+              <Col md={size.step[1]} lg={size.step[1]} xl={size.step[1]} xxl={size.step[1]}>
+                Pay the coins to target account.
+              </Col>
+
+              <Col className='' md={size.normal[0]} lg={size.normal[0]} xl={size.normal[0]} xxl={size.normal[0]}>
+              </Col>
+              <Col className='text-end' md={size.normal[1]} lg={size.normal[1]} xl={size.normal[1]} xxl={size.normal[1]}>
+                <button className='btn btn-md btn-primary'>Pay</button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Tab>
+    </Tabs>
+
   );
 }
 export default BountySubmit;
--- market/src/component/bounty_target.js
+++ market/src/component/bounty_target.js
@@ -1,8 +1,10 @@
 import { Row, Col } from 'react-bootstrap';
 import { useEffect, useState } from "react";
+import { FaSkullCrossbones } from "react-icons/fa";
 
-import TPL from "../system/tpl";
+import tools from "../lib/tools";
 
+let series= null;
 function BountyTarget(props) {
   const size = {
     row: [12],
@@ -10,33 +12,87 @@ function BountyTarget(props) {
     step: [2, 10],
     head: [4, 8],
     normal: [9, 3],
+    bonus: [3, 4, 4, 1],
   };
 
   let [list, setList] = useState([]);
+  
+  const single = {
+    series: 0,
+    bonus: 100,
+    amount: 10
+  };
 
   const self = {
-
+    clickAdd: (ev) => {
+      const nlist = tools.clone(list);
+      nlist.push(tools.clone(single));
+      setList(nlist);
+    },
+    clickRemove: (index) => {
+      const nlist = [];
+      for (let i = 0; i < list.length; i++) {
+        if (i !== index) nlist.push(list[i]);
+      }
+      setList(nlist);
+    },
   }
 
   useEffect(() => {
-  }, []);
+    if(props.data && props.data.series){
+      console.log(props.data.series);
+      series = props.data.series;
+    }
+  }, [props.data]);
 
   return (
     <Row>
       <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
         <hr />
       </Col>
-      {list.map((row, index) => (
-        <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-          <Row>
-            <Col key={index} md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} >
+      <Col md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
 
+        <Row hidden={list.length === 0} className='pt-1'>
+          <Col md={size.bonus[0]} lg={size.bonus[0]} xl={size.bonus[0]} xxl={size.bonus[0]} >Series</Col>
+          <Col md={size.bonus[1]} lg={size.bonus[1]} xl={size.bonus[1]} xxl={size.bonus[1]} >Bonus</Col>
+          <Col md={size.bonus[2]} lg={size.bonus[2]} xl={size.bonus[2]} xxl={size.bonus[2]} >Amount</Col>
+          <Col md={size.bonus[3]} lg={size.bonus[3]} xl={size.bonus[3]} xxl={size.bonus[3]} ></Col>
+        </Row>
+
+        {list.map((row, index) => (
+          <Row key={index} className='pt-1'>
+            <Col md={size.bonus[0]} lg={size.bonus[0]} xl={size.bonus[0]} xxl={size.bonus[0]} >
+              <select className='form-control' onChange={(ev) => {
+
+              }}>
+                {series!==null && series.map((ss, order) => (
+                  <option key={order} value={order}>#{order} {ss.name}</option>
+                ))}
+              </select>
+            </Col>
+            <Col md={size.bonus[1]} lg={size.bonus[1]} xl={size.bonus[1]} xxl={size.bonus[1]} >
+              <input className='form-control' type="number" value={row.bonus} onChange={(ev) => {
+
+              }} />
+            </Col>
+            <Col md={size.bonus[2]} lg={size.bonus[2]} xl={size.bonus[2]} xxl={size.bonus[2]} >
+              <input className='form-control' type="number" value={row.amount} onChange={(ev) => {
+
+              }} />
+            </Col>
+            <Col md={size.bonus[3]} lg={size.bonus[3]} xl={size.bonus[3]} xxl={size.bonus[3]} >
+              <button className='btn btn-sm btn-danger' onClick={(ev) => {
+                self.clickRemove(index);
+              }}><FaSkullCrossbones /></button>
             </Col>
           </Row>
-        </Col>
-      ))}
-      <Col className='text-center' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-        <button className='btn btn-md btn-primary'>+ bounty target</button>
+        ))}
+
+      </Col>
+      <Col className='text-center pt-1' md={size.row[0]} lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
+        <button className='btn btn-md btn-primary' onClick={(ev) => {
+          self.clickAdd();
+        }}>+ bounty target</button>
       </Col>
     </Row>
   );
