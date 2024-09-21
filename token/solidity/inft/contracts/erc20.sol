425bd48667e2bbfcd242cda228f28d2b45a4c605
fuu <fu@yihaofang.com>
2024-09-22 07:31:34 +0800

    1.trying of deploy ERC20

--- token/solidity/inft/contracts/erc20.sol
+++ token/solidity/inft/contracts/erc20.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.27;
 
 // Importing OpenZeppelin's ERC20 implementation
 // npm install @openzeppelin/contracts
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Define the INFT token contract
-contract INFT is ERC20 {
+contract InftToken is ERC20 {
     // Constructor function to initialize the token with a name and symbol
-    constructor(uint256 initialSupply) ERC20("INFT", "INFT") {
+    constructor(uint256 initialSupply) ERC20("InftToken", "INFT") {
         // Mint the initial supply of tokens to the deployer's address
         _mint(msg.sender, initialSupply * (20000000 ** decimals()));
     }
--- token/solidity/inft/migrations/2_deploy_contracts.js
+++ token/solidity/inft/migrations/2_deploy_contracts.js
@@ -1,4 +1,32 @@
-const MyContract = artifacts.require("MyContract");
+
+/*TEST: deploy the basic contract on holesky*/
+const MyContract = artifacts.require("InftToken");
 module.exports = function(deployer) {
-  deployer.deploy(MyContract, "Hello, World!");
+  deployer.deploy(MyContract,20000000);
 };
+
+// const MyContract = artifacts.require("Inft");
+// module.exports = function(deployer) {
+//   deployer.deploy(MyContract,20000000);
+// };
+
+// async function main() {
+//   const [deployer] = await ethers.getSigners();
+
+//   console.log("Deploying contracts with the account:", deployer.address);
+
+//   const balance = await deployer.getBalance();
+//   console.log("Account balance:", balance.toString());
+
+//   const Token = await ethers.getContractFactory("MyToken");
+//   const token = await Token.deploy("MyToken", "MTK", 1000000); // Name, Symbol, Initial Supply (1 million)
+
+//   console.log("Token deployed to:", token.address);
+// }
+
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
